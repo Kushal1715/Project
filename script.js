6a53323398aa09d5c9f90920550de5acf7468737
Kushal1715 <kushalkhadka77@gmail.com>
2023-08-18 12:27:21 +0545

    product page updated

--- script.js
+++ script.js
@@ -1,7 +1,68 @@
-let navbar = document.querySelector(".nav");
-let span = document.querySelector(".span");
 
-window.addEventListener("scroll", ()=>{
-    navbar.classList.add("scroll");
-    span.style.color = "#00fff3";
-})
+let p1 = document.querySelector("#p1");
+let p2 = document.querySelector("#p2");
+let p3 = document.querySelector("#p3");
+let p4 = document.querySelector("#p4");
+let p5 = document.querySelector("#p5");
+let p6 = document.querySelector("#p6");
+let p7 = document.querySelector("#p7");
+let p8 = document.querySelector("#p8");
+let p9 = document.querySelector("#p9");
+let p10 = document.querySelector("#p10");
+let p11 = document.querySelector("#p11");
+let p12 = document.querySelector("#p12");
+let p13 = document.querySelector("#p13");
+let p14 = document.querySelector("#p14");
+let p15 = document.querySelector("#p15");
+let p16 = document.querySelector("#p16");
+
+
+p1.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+
+p2.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p3.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p4.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p5.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p6.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p7.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p8.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p9.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p10.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p11.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p12.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p13.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p14.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p15.onclick = function(){
+    window.location.href = "productDetails.html";
+}
+p16.onclick = function(){
+    window.location.href = "productDetails.html";
+}
